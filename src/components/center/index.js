bb78fc249d2ee20949fb369020c81f7f8ef9a065
Ben Hopkins <bjhops17@gmail.com>
2021-02-15 13:05:18 -0800

    added styling and added responsiveness

--- src/components/center/index.js
+++ src/components/center/index.js
@@ -4,21 +4,22 @@ import React, { Component } from "react";
 export default class Center extends Component {
   render() {
     return (
-     
-      <div className="hexcontain" style={{ margin: "25vw 0vw 25vw 0vw" }}>
-        <div className="tophex" style={{ margin: "3vw 0vw 0vw 0vw" }}>
+     <section className="center-back">
+      <div className="hexcontain">
+        <div className="tophex">
           <div className="hexagon"></div>
           <div className="hexagon" style={{ margin: "0vw 0vw .5vw 2vw" }}></div>
           <div className="hexagon" style={{ margin: "0vw 0vw .5vw 2vw" }}></div>
         </div>
         <div className="centerhex">
-          <div className="hexagon" style={{ margin: "5.5vw 0vw 0vw 0vw" }}></div>
-          <div className="hexagon" style={{ margin: "5.5vw 0vw .5vw 2vw" }}></div>
+          <div className="hexagon adjust" style={{ margin: "5.5vw 0vw 0vw 0vw" }}></div>
+          <div className="hexagon adjust" style={{ margin: "5.5vw 0vw .5vw 2vw" }}></div>
         </div>
         <div className="lowerhex">
-          <div className="hexagon" style={{ margin: "5.5vw 0vw 0vw 0vw" }}></div>
+          <div className="hexagon adjust" style={{ margin: "5.5vw 0vw 0vw 0vw" }}></div>
         </div>
       </div>
+      </section>
       
     
     );
