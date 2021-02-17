6e49e407ffe28690e31b4781d7c7c2872ff88978
Ben Hopkins <bjhops17@gmail.com>
2021-02-17 07:15:38 -0800

    added About page fetures

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./style.css"
 
 export default function About() {
-    return (
-        <div>
-            
+  return (
+    <div className="">
+      <div className="row bio">
+        <div className="col s10 m6 l4 test">
+          <div className="card-panel">
+              
+              <img className="me" src="http://placekitten.com/g/200/300" />
+              
+              
+            <p className="white-text">
+              Hello! I hail from a small town two hours east of Seattle. I am currently working to complete the UW Coding Bootcamp with the goal of entering into the tech industry! My employment history has never included anything tech related (as of yet) so I will not mention former accomplishments. I did graduate with my BA in Criminal Justice back in 2014 but never really used it in the work force. I am excited to launch into a tech career and learn as much as I can! My porfolio page has examples of my work and I will continue to update it with new projects as they are completed.
+              </p>
+           
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
