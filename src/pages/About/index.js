9a9652643f100fa636bb260e676dcb76e6b8f157
Ben Hopkins <bjhops17@gmail.com>
2021-04-14 10:40:34 -0700

    added proffessional pic

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css"
+import pic from "./profilepic.JPG";
 
 export default function About() {
   return (
@@ -7,19 +8,23 @@ export default function About() {
       <div className="row bio">
         <div className="col s10 m6 l4 test">
           <div className="card-panel">
+            <div className="ratio">
               
-              <img className="me" src="http://placekitten.com/g/200/300" />
+              <img className="me" src={pic} />
+
+            </div>
               
               
             <p className="white-text">
               Hello! I hail from a small town two hours east of Seattle and I recently graduated from University of Washingtons Full Stack Web Development Bootcamp (thats a mouth full)! My employment history has never included anything tech related (as of yet) so I will not mention former accomplishments. I did graduate with my BA in Criminal Justice back in 2014 but never really used it in the work force (accept for a short gig in the freezing waste lands of northern Alaska). I am excited to launch into a tech career and learn as much as I can! Curently, I can work with: 
+              <br></br>
               <li>Javascript</li>
               <li>JQuery</li>
               <li>CSS</li>
               <li>HTML</li>
               <li>React.js</li>
               <li>Node.js</li>
-              <li>mySql via sequelize</li>
+              <li>MySql via sequelize</li>
               <li>mongoDB via mongoose</li>
               
               </p>
